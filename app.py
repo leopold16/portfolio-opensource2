8a811a224de6d79a509e66d51861e0889caa98a8
leopold16 <lw2999@columbia.edu>
2024-10-30 01:50:38 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/project1')
+def project1():
+    return render_template('project1.html')
+
+@app.route('/project2')
+def project2():
+    return render_template('project2.html')
+
+@app.route('/project3')
+def project3():
+    return render_template('project3.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,32 @@
+const roles = ["AI fascinated", "future oriented", "using technology to solve problem"];
+let index = 0;
+let charIndex = 0;
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const delayBetweenRoles = 2000;
+const roleTextElement = document.getElementById("role-text");
+
+function typeRole() {
+    if (charIndex < roles[index].length) {
+        roleTextElement.textContent += roles[index].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeRole, typingSpeed);
+    } else {
+        setTimeout(eraseRole, delayBetweenRoles);
+    }
+}
+
+function eraseRole() {
+    if (charIndex > 0) {
+        roleTextElement.textContent = roles[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseRole, erasingSpeed);
+    } else {
+        index = (index + 1) % roles.length;
+        setTimeout(typeRole, typingSpeed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(typeRole, delayBetweenRoles);
+});
